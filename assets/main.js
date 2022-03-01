07964755d92e90a3f6889bec95b2d802b6fe9622
Julio Cesar Castro Macias <jcastrom1601@alumno.ipn.mx>
2022-02-28 20:57:53 -0600

    update general

---
+++ assets/main.js
@@ -0,0 +1,42 @@
+const flavors = ['chocolate', 'strawberry', 'vanilla', 'natural'];
+const decorations = ['choco', 'whipped', 'candle', 'colored'];
+
+//MOSTRAR O ESCONDER RANGO DE 'CANDLE'
+document.getElementById('candle').onclick = function(){
+    if (this.checked){
+        document.getElementById('candleRange').type = 'range';
+        document.getElementsByClassName('rangeText').textContent = this.value;
+    }else {
+        document.getElementById('candleRange').type = 'hidden';
+    }
+}
+
+//MOSTRAR RANGO DE SLIDER DE 'CANDLE'
+function checkSlider(rangeText,candleCheck) {
+    document.getElementById('rangeText').innerHTML = document.getElementById('candleRange').value;    
+}
+
+//GENERAR ORDEN/PEDIDO
+document.getElementById('buttonOrder').onclick = function() {
+    let subtotal = 0;
+    flavors.forEach(flavor => {
+        if (document.getElementById(flavor).checked){
+            subtotal += parseInt(document.getElementById(flavor).value);
+        }
+    });
+
+    decorations.forEach(decoration => {
+        if (document.getElementById(decoration).checked){
+            subtotal += parseInt(document.getElementById(decoration).value);
+        }
+    });
+    console.log(subtotal)
+
+    window.location.assign('webapp/cart.html');
+    
+    document.write("<!DOCTYPE html>");
+    document.write("<html>");
+    document.write("</html>");
+}
+
+
